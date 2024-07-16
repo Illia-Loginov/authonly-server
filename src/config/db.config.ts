41236db0fa21302ad4d149aff029d8d975578918
Illia Loginov <illia.loginov.g@gmail.com>
2024-07-16 16:45:18 +0300

    Use PoolConfig type in db config for safety and code completion

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,8 +1,9 @@
+import type { PoolConfig } from 'pg';
 import { validateDbConfig } from './envValidation.js';
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = validateDbConfig();
 
-export const pgPoolConfig = {
+export const pgPoolConfig: PoolConfig = {
   user: DB_USER,
   password: DB_PASSWORD,
   host: DB_HOST,
