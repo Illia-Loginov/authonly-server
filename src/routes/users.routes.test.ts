f352d1f50d56e663bcb287098d1625062cb7c1b5
Illia Loginov <illia.loginov.g@gmail.com>
2024-11-03 19:05:40 +0200

    Test DELETE /users/:id

--- src/routes/users.routes.test.ts
+++ src/routes/users.routes.test.ts
@@ -1,4 +1,4 @@
-import { after, describe, it } from 'node:test';
+import { after, before, describe, it } from 'node:test';
 import { strict as assert } from 'node:assert';
 import request from 'supertest';
 import app from '../app.js';
@@ -228,6 +228,136 @@ describe('GET /users', () => {
       .deleteFrom('users')
       .where('username', '=', uniqueValidUserCreds.username)
       .execute();
+  });
+});
+
+describe('DELETE /users/:id', async () => {
+  const users: { cookie: string; id: string }[] = [];
+
+  before(async () => {
+    for (let userIndex = 0; userIndex < 2; userIndex++) {
+      const response = await request(app)
+        .post('/users')
+        .send({
+          username: `${Date.now()}_${userIndex}`,
+          password: Date.now().toString()
+        });
+
+      const cookie = response
+        .get('Set-Cookie')
+        ?.find((cookie) => cookie.startsWith(cookieName));
+
+      const id = response.body?.user?.id;
+
+      assert.ok(
+        typeof cookie === 'string',
+        'POST /users did not return cookie'
+      );
+      assert.ok(typeof id === 'string', 'POST /users did not return id');
+
+      users.push({ cookie, id });
+    }
+  });
+
+  it('Rejects, when not authenticated', async () => {
+    const response = await request(app).delete(`/users/${users[1]?.id}`);
+
+    assert.equal(response.status, 401, 'Invalid status code');
+    assert.equal(
+      response.body.message,
+      'No valid session token cookie provided'
+    );
+  });
+
+  it('Rejects, when using incorrect session token', async () => {
+    const response = await request(app)
+      .delete(`/users/${users[1]?.id}`)
+      .set('Cookie', [`${cookieName}=incorrect_session_token`]);
+
+    assert.equal(response.status, 401, 'Invalid status code');
+    assert.equal(
+      response.body.message,
+      'No valid session token cookie provided'
+    );
+  });
+
+  it('Rejects, when trying to delete another user', async () => {
+    const response = await request(app)
+      .delete(`/users/${users[1]?.id}`)
+      .set('Cookie', [users[0]?.cookie || '']);
+
+    assert.equal(response.status, 403, 'Invalid status code');
+    assert.equal(response.body.message, 'Cannot delete other users');
+  });
+
+  it('Rejects, when using invalid id', async () => {
+    const response = await request(app)
+      .delete(`/users/INVALID_ID`)
+      .set('Cookie', [users[0]?.cookie || '']);
+
+    assert.equal(response.status, 400, 'Invalid status code');
+
+    const { message, issues } = response.body;
+
+    assert.equal(message, 'Invalid input', 'Invalid message');
+    assert.deepEqual(issues, {
+      id: ['Invalid uuid']
+    });
+  });
+
+  it('Deletes users on valid input', async () => {
+    for (const { cookie, id } of users) {
+      const response = await request(app)
+        .delete(`/users/${id}`)
+        .set('Cookie', [cookie]);
+
+      assert.equal(response.status, 200, 'Invalid status code');
+
+      const { user: responseUser } = response.body;
+
+      assert.deepEqual(
+        responseUser,
+        fullUserSchema
+          .pick({ id: true, username: true, created_at: true })
+          .parse(responseUser),
+        'Invalid response body'
+      );
+
+      assert.equal(responseUser.id, id, 'Invalid id');
+    }
+
+    const ids = users.map(({ id }) => id);
+
+    const [nonDeletedUsersCount, nonDeletedSessionsCount] = await Promise.all([
+      db
+        .selectFrom('users')
+        .select(({ fn }) => [fn.countAll().as('count')])
+        .where('id', 'in', ids)
+        .execute(),
+      db
+        .selectFrom('sessions')
+        .select(({ fn }) => [fn.countAll().as('count')])
+        .where('user', 'in', ids)
+        .execute()
+    ]);
+
+    assert.equal(
+      Number(nonDeletedUsersCount[0]?.count),
+      0,
+      'Users were not deleted'
+    );
+    assert.equal(
+      Number(nonDeletedSessionsCount[0]?.count),
+      0,
+      'Sessions were not deleted'
+    );
+  });
+
+  after(async () => {
+    const ids = users.map(({ id }) => id);
+
+    await db.deleteFrom('users').where('id', 'in', ids).execute();
+    await db.deleteFrom('sessions').where('user', 'in', ids).execute();
   });
 });
 
